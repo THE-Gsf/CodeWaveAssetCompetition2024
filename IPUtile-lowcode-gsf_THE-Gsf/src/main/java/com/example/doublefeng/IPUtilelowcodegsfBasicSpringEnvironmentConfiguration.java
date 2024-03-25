4932b7a11821587eb9b6a22872c65e3c204f97b1
guoshuangfeng01 <1475899565@qq.com>
2024-03-25 16:23:32 +0800

    初次提交

---
+++ IPUtile-lowcode-gsf_THE-Gsf/src/main/java/com/example/doublefeng/IPUtilelowcodegsfBasicSpringEnvironmentConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.doublefeng;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 加入spring环境配置（在spring.factories中指定）
+ */
+@Configuration
+@ComponentScan(basePackageClasses = LibraryAutoScan.class)
+public class IPUtilelowcodegsfBasicSpringEnvironmentConfiguration {
+}
